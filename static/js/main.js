a7945b9383b4c5c82f1545d0a42cabdb0e077d6d
Namhyeok <dr_lunars@naver.com>
2021-05-31 01:09:59 +0900

    [modify files] update

--- static/js/main.js
+++ static/js/main.js
@@ -5,19 +5,27 @@ function getBotResponse() {
     $("#textInput").val("");
     $("#chatbox").append(userHtml);
     document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    var objDiv = document.getElementById("chatbox");
+    objDiv.scrollTop = objDiv.scrollHeight;
     $.get("/get", { msg: rawText }).done(function(data) {
       var botHtml = '<p class="botText"><span>' + data + '</span></p>';
       $("#chatbox").append(botHtml);
       document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+      var objDiv = document.getElementById("chatbox");
+      objDiv.scrollTop = objDiv.scrollHeight;
     });
   }
   $("#textInput").keypress(function(e) {
       if ((e.which == 13) && document.getElementById("textInput").value != "" ){
           getBotResponse();
+          var objDiv = document.getElementById("chatbox");
+          objDiv.scrollTop = objDiv.scrollHeight;
       }
   });
   $("#buttonInput").click(function() {
       if (document.getElementById("textInput").value != "") {
           getBotResponse();
+          var objDiv = document.getElementById("chatbox");
+          objDiv.scrollTop = objDiv.scrollHeight;
       }
   })
