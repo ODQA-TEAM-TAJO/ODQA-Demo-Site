afed822bbfc924c94e7d3e8e5d02ea52179a0715
Namhyeok <dr_lunars@naver.com>
2021-05-30 14:14:36 +0900

    [add files] ramake

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chatbot.urls')),
-]
